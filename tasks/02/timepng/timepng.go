c5b7901965081278a55ac12008dde037e485301c
Daria <ryzhenkova.ds@phystech.edu>
2021-05-23 19:27:38 +0300

    yes

--- tasks/02/timepng/timepng.go
+++ tasks/02/timepng/timepng.go
@@ -3,27 +3,52 @@ package timepng
 import (
 	"image"
 	"image/color"
+	"image/png"
 	"io"
 	"time"
 )
 
 // TimePNG записывает в `out` картинку в формате png с текущим временем
 func TimePNG(out io.Writer, t time.Time, c color.Color, scale int) {
-	// TODO: Implement me
+	_ = png.Encode(out, buildTimeImage(t, c, scale))
 }
 
 // buildTimeImage создает новое изображение с временем `t`
 func buildTimeImage(t time.Time, c color.Color, scale int) *image.RGBA {
-	// TODO: Implement me
-	return nil
+	img := image.NewRGBA(image.Rect(0, 0, (X*5+4)*scale, Y*scale))
+	T := t.Format(Layout)
+	var runes [5]rune
+	for i, s := range T {
+		runes[i] = s
+	}
+	fillWithMask(img.SubImage(image.Rect(0, 0, X*scale, Y*scale)).(*image.RGBA), nums[runes[0]], c, scale)
+	for i := 1; i < 5; i++ {
+		fillWithMask(img.SubImage(image.Rect(i*(X+1)*scale, 0, (X+i*(X+1))*scale, Y*scale)).(*image.RGBA), nums[runes[i]], c, scale)
+	}
+	return img
 }
 
 // fillWithMask заполняет изображение `img` цветом `c` по маске `mask`. Маска `mask`
 // должна иметь пропорциональные размеры `img` с учетом фактора `scale`
 // NOTE: Так как это вспомогательная функция, можно считать, что mask имеет размер (3x5)
 func fillWithMask(img *image.RGBA, mask []int, c color.Color, scale int) {
-	// TODO: implement me
+	for x := 0; x < X; x++ {
+		for y := 0; y < Y; y++ {
+			if mask[x+y*3] == 1 {
+				for i := 0; i < scale; i++ {
+					for j := 0; j < scale; j++ {
+						img.Set((x*scale+i)+img.Bounds().Min.X, y*scale+j, c)
+					}
+				}
+			}
+		}
+	}
 }
+
+const Layout = "15:04"
+
+const X = 3
+const Y = 5
 
 var nums = map[rune][]int{
 	'0': {
--- tasks/02/urlshortener/shortener.go
+++ tasks/02/urlshortener/shortener.go
@@ -1,25 +1,61 @@
 package urlshortener
 
 import (
+	"github.com/go-chi/chi"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 type URLShortener struct {
-	// TODO: More fields?
+	urls map[string]string
 	addr string
 }
 
 func NewShortener(addr string) *URLShortener {
+	rand.Seed(time.Now().UnixNano())
+	var m map[string]string
+	m = make(map[string]string)
 	return &URLShortener{
-		// TODO: Initialization for more fields?
+		urls: m,
 		addr: addr,
 	}
 }
 
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
 func (s *URLShortener) HandleSave(rw http.ResponseWriter, req *http.Request) {
-	// TODO: Implement me
+	str := req.URL.Query().Get("u")
+	_, err := url.Parse(str)
+	if err != nil {
+		http.Error(rw, "", http.StatusBadRequest)
+		return
+	}
+
+	h := randSeq(10)
+	if _, emp := s.urls[h]; emp {
+		http.Error(rw, "", http.StatusInternalServerError)
+		return
+	}
+
+	s.urls[h] = str
+	_, _ = rw.Write([]byte(s.addr + "/" + h))
 }
 
 func (s *URLShortener) HandleExpand(rw http.ResponseWriter, req *http.Request) {
-	// TODO: Implement me
+	str := chi.URLParam(req, "key")
+	if s.urls[str] == "" {
+		http.Error(rw, "", http.StatusNotFound)
+	}
+
+	http.Redirect(rw, req, s.urls[str], http.StatusMovedPermanently)
 }
